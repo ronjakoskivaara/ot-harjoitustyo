7b0b65a9906b98d99b737f5cd27322e614795121
Ronja Koskivaara <ronja.koskivaara@gmail.com>
2020-04-06 20:45:41 +0300

    Luotu package ui ja uusi main class javafx:ää varten

--- PaassalaskuHarjoitus/src/main/java/PaassalaskuHarjoitus/Main.java
+++ PaassalaskuHarjoitus/src/main/java/PaassalaskuHarjoitus/Main.java
@@ -1,6 +1,7 @@
 package PaassalaskuHarjoitus;
 
 import PaassalaskuHarjoitus.domain.Sum;
+import PaassalaskuHarjoitus.ui.PaassalaskuHarjoitusUi;
 import java.util.Scanner;
 
 /**
@@ -9,24 +10,25 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) { 
-        Scanner reader = new Scanner(System.in); 
-        System.out.println("Tervetuloa päässälaskuharjoitteluun");
-        System.out.println("Lopettaaksesi harjoittelu paina enter");
-        
-        boolean help = true;
-        
-        while(help) {
-            Sum sumCalculation = new Sum();
-            sumCalculation.printCalculation();
-            String answer = reader.nextLine();
-            if (answer.equals("")) {
-                help = false;
-            }
-            else {
-                sumCalculation.compareAnswer(answer);
-            }
-        }
-        System.out.println("Kiitos!");
+          PaassalaskuHarjoitusUi.main(args);
+//        Scanner reader = new Scanner(System.in); 
+//        System.out.println("Tervetuloa päässälaskuharjoitteluun");
+//        System.out.println("Lopettaaksesi harjoittelu paina enter");
+//        
+//        boolean help = true;
+//        
+//        while(help) {
+//            Sum sumCalculation = new Sum();
+//            sumCalculation.printCalculation();
+//            String answer = reader.nextLine();
+//            if (answer.equals("")) {
+//                help = false;
+//            }
+//            else {
+//                sumCalculation.compareAnswer(answer);
+//            }
+//        }
+//        System.out.println("Kiitos!");
         
         
 
---
+++ PaassalaskuHarjoitus/src/main/java/PaassalaskuHarjoitus/ui/PaassalaskuHarjoitusUi.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PaassalaskuHarjoitus.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author koskiron
+ */
+public class PaassalaskuHarjoitusUi extends Application {
+    
+    @Override
+    public void start(Stage ikkuna) {
+        ikkuna.setTitle("Hei Maailma!");
+        ikkuna.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
